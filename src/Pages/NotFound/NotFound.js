7283e4bbef87aa5f70ded556654a47065de3524c
Dev-Zual <freelancerzual@gmail.com>
2022-05-23 18:39:12 +0600

    created 404 page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import img from '../../images/404-page.jpg';
 const NotFound = () => {
-  return <div className="flex justify-center items-center"></div>;
+  return (
+    <div className="flex justify-center items-center">
+      <img src={img} alt="" />
+    </div>
+  );
 };
 
 export default NotFound;
